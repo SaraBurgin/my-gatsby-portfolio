1fd28d98461df73b739dbac6020675656a077a71
Sara <saragonzalezburgin@hotmail.com>
2020-10-12 14:51:44 +0200

    Add TODO list image

--- src/mock/data.js
+++ src/mock/data.js
@@ -95,7 +95,7 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'reacttodolist.png',
+    img: 'todolist.png',
     title: 'React TODO List.',
     info: 'React TODO List built with React.js.',
     info2: `Tech stack: 
